b2bdde66dde60431b788e9fde3b369e839df2075
Omar Crisostomo Barrera <omar13cb@gmail.com>
2026-04-11 22:07:31 -0600

    responsive

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,9 +1,4 @@
 function toggleMenu() {
     const sidebar = document.getElementById("sidebar");
-
-    if (sidebar.style.left === "0px") {
-        sidebar.style.left = "-250px";
-    } else {
-        sidebar.style.left = "0px";
-    }
+    sidebar.classList.toggle("active");
 }
--- static/js/slider.js
+++ static/js/slider.js
@@ -1,7 +1,7 @@
 new Swiper(".mySwiper", {
-    slidesPerView: 1,
     centeredSlides: true,
     spaceBetween: 20,
+    loop: true,
 
     pagination: {
         el: ".swiper-pagination",
@@ -9,11 +9,17 @@ new Swiper(".mySwiper", {
     },
 
     breakpoints: {
+        0: {
+            slidesPerView: 1.2 // 🔥 móvil: uno principal + peek lateral
+        },
+        640: {
+            slidesPerView: 1.5
+        },
         768: {
-            slidesPerView: 2,
+            slidesPerView: 2 // tablet
         },
         1024: {
-            slidesPerView: 3,
+            slidesPerView: 3 // desktop
         }
     }
 });
