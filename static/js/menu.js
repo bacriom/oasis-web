ba7cd61bdfc7afb9c03a754ba45a04fdfffecea1
Omar Crisostomo Barrera <omar13cb@gmail.com>
2026-04-11 22:08:36 -0600

    menu

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,4 +1,16 @@
-function toggleMenu() {
+function toggleMenu(e) {
+    e.stopPropagation(); // evita que el click se propague
     const sidebar = document.getElementById("sidebar");
     sidebar.classList.toggle("active");
 }
+
+// Cerrar al hacer click fuera
+document.addEventListener("click", function() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.remove("active");
+});
+
+// Evita que clicks dentro del menú lo cierren
+document.getElementById("sidebar").addEventListener("click", function(e) {
+    e.stopPropagation();
+});
